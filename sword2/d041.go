a2ac350c2b9e8b927ebb8e95328a3512378dfd5e
luhuimin <1767014626@qq.com>
2022-07-16 16:45:26 +0800

    sword2 剑指 Offer II 041. 滑动窗口的平均值

---
+++ sword2/d041.go
@@ -0,0 +1,32 @@
+package sword2
+
+type MovingAverage struct {
+	q []int
+    size int
+	sum int
+}
+
+/** Initialize your data structure here. */
+func Constructor(size int) MovingAverage {
+	return MovingAverage{
+		size: size,
+
+	}
+}
+
+func (this *MovingAverage) Next(val int) float64 {
+	if len(this.q)==this.size {
+		this.sum-=this.q[0]
+		this.q=this.q[1:]
+	}
+	this.sum+=val
+	this.q=append(this.q,val)
+
+	return float64(this.sum) / float64(len(this.q))
+}
+
+/**
+ * Your MovingAverage object will be instantiated and called as such:
+ * obj := Constructor(size);
+ * param_1 := obj.Next(val);
+ */
